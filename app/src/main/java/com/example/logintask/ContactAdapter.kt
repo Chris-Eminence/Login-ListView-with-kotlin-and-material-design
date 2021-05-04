ad94580096a25973c59fa4f0635986bd44eceea3
Chris-Eminence <emichris380@gmail.com>
2021-05-04 17:00:00 +0100

    :)

--- app/src/main/java/com/example/logintask/ContactAdapter.kt
+++ app/src/main/java/com/example/logintask/ContactAdapter.kt
@@ -1,3 +1,22 @@
 package com.example.logintask
 
-class ContactAdapter : Adapter
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ContactAdapter : RecyclerView.Adapter<ContactAdapter.ContactViewHolder>(){
+
+    inner class ContactViewHolder : RecyclerView.ViewHolder()
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
