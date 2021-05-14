83cc361dbf2f9f8565456b698b416ca08b619389
Chris-Eminence <emichris380@gmail.com>
2021-05-14 16:31:12 +0100

    second commit

---
+++ app/src/main/java/com/example/logintask/DummyData.kt
@@ -0,0 +1,36 @@
+package com.example.logintask
+
+object DummyData {
+    val facts = listOf(
+        FactModel(
+            "Kotlin",
+            R.drawable.kotlin,
+            "Kotlin is an expressive and concise programming language that reduces common errors. Easily integrates into existing apps and is 100% interoperable with the Java language. Big community. Easy to learn. Online courses. Write less code. Multiplatform. Code safely."
+        ),
+
+        FactModel(
+            "Java",
+            R.drawable.java,
+            "Java is fundamentally object-oriented. The code is so robust because Java objects contain no references to data external to themselves. The language is considered to be very simple; however, it comes with a library of classes that offer commonly used utility functions that most Java programs can't work without."
+        ),
+
+        FactModel(
+            "Python",
+            R.drawable.python,
+            "Python is often used as a support language for software developers, for build control and management, testing, and in many other ways."
+
+            ),
+
+        FactModel(
+            "JavaScript",
+            R.drawable.javascript,
+            "JavaScript is one of the three main things of the www (World Wide Web). It enables interactive web pages and thus is an essential part of web applications. A majority of websites use it and all major web browsers have a devoted JavaScript engine to execute it. JavaScript is single threaded."
+        ),
+
+        FactModel(
+            "Php",
+            R.drawable.php,
+            "PHP stands for Hypertext Preprocessor and is a server-side programming language. ... A good benefit of using PHP is that it can interact with many different database languages including MySQL. We work with MySQL at Bluelinemedia since this is also a free language so it makes sense to use PHP."
+        )
+    )
+}
---
+++ app/src/main/java/com/example/logintask/FactModel.kt
@@ -0,0 +1,4 @@
+package com.example.logintask
+
+data class FactModel(val name : String, val logo : Int, val fact: String) {
+}
